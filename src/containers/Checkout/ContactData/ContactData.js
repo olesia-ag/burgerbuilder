80a8022e868857deb18ab19f15eb6f3bb7d79667
Olesia.G <olgarbuzova@gmail.com>
2020-06-10 19:03:17 -0400

    added form handlers

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,66 +3,163 @@ import Button from '../../../components/UI/Button/Button'
 import classes from './ContactData.module.css'
 import axiosIns from '../../../axios-orders'
 import Spinner from '../../../components/UI/Spinner/Spinner'
+import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends React.Component {
 	state = {
-		name: '',
-		email: '',
-		addres: {
-			street: '',
-			postalCode: '',
+		orderForm: {
+			name: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Your Name',
+				},
+				value: '',
+				validation: {
+					required: true,
+				},
+				valid: false,
+			},
+			street: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Street',
+				},
+				value: '',
+				validation: {
+					required: true,
+				},
+				valid: false,
+			},
+			zipCode: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Zip Code',
+				},
+				value: '',
+				validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+				},
+				valid: false,
+			},
+			country: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Country',
+				},
+				value: '',
+				validation: {
+					required: true,
+				},
+				valid: false,
+			},
+			email: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'email',
+					placeholder: 'Your email',
+				},
+				value: '',
+				validation: {
+					required: true,
+				},
+				valid: false,
+			},
+			deliveryMethod: {
+				elementType: 'select',
+				elementConfig: {
+					options: [
+						{ value: 'fastest', displayValue: 'Fastest' },
+						{ value: 'cheapest', displayValue: 'Cheapest' },
+					],
+				},
+                value: '',
+               
+			},
 		},
 		loading: false,
 	}
 
 	orderHandler = (event) => {
 		event.preventDefault()
-		console.log('ord', this.props.ingredients)
+
 		this.setState({ loading: true })
+		const formData = {}
+		for (let formElementIdentifier in this.state.orderForm) {
+			formData[formElementIdentifier] = this.state.orderForm[
+				formElementIdentifier
+			].value
+		}
 		const order = {
 			ingredients: this.props.ingredients,
 			//in production price shoulb be calculate on the server to make sure that user doesn't manipulate it
 			price: this.props.totalPrice,
 			deliveryMethod: 'fastest',
+			orderData: formData,
 		}
-        //.json for firebase endpoint specifically!!!
-        console.log('order final:', order)
+		//.json for firebase endpoint specifically!!!
+
 		axiosIns
 			.post('/orders.json', order)
-            .then((res) => {
-            this.setState({ loading: false })
-            this.props.history.push('/')
-            })
+			.then((res) => {
+				this.setState({ loading: false })
+				this.props.history.push('/')
+			})
 			.catch((err) => this.setState({ loading: false }))
 	}
 
+	checkValidity(value, rules) {
+		let isValid = true
+		if (rules.required) {
+			isValid = value.trim() !== '' && isValid
+		}
+		if (rules.minLength) {
+			isValid = value.length >= rules.minLength && isValid
+        }
+        if (rules.minLength) {
+			isValid = value.length <= rules.maxLength && isValid
+		}
+		return isValid
+	}
+
+	inputChangedHandler = (event, inputIdentifier) => {
+		const updatedOrderForm = {
+			...this.state.orderForm,
+		}
+		const updatedFormElement = { ...updatedOrderForm[inputIdentifier] }
+		updatedFormElement.value = event.target.value
+		updatedFormElement.valid = this.checkValidity(
+			updatedFormElement.value,
+			updatedFormElement.validation
+		)
+		updatedOrderForm[inputIdentifier] = updatedFormElement
+		console.log(updatedFormElement)
+		this.setState({ orderForm: updatedOrderForm })
+	}
 	render() {
+		const formElementsArray = []
+		for (let key in this.state.orderForm) {
+			formElementsArray.push({ id: key, config: this.state.orderForm[key] })
+		}
 		let form = (
-			<form>
-				<input
-					className={classes.Input}
-					type='text'
-					name='name'
-					placeholder='your name'
-				/>
-				<input
-					className={classes.Input}
-					type='email'
-					name='email'
-					placeholder='your email'
-				/>
-				<input
-					className={classes.Input}
-					type='text'
-					name='street'
-					placeholder='your street'
-				/>
-				<input
-					className={classes.Input}
-					type='text'
-					name='postal'
-					placeholder='your code'
-				/>
+			<form onsubmit={this.orderHandler}>
+				{formElementsArray.map((formElement) => (
+					<Input
+						key={formElement.id}
+						elementType={formElement.config.elementType}
+						elementConfig={formElement.config.elementConfig}
+						value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)}
+                        invalid={!formElement.config.valid}
+                        shouldValidate = {formElement.config.validation}
+					/>
+				))}
+
 				<Button btnType='Success' clicked={this.orderHandler}>
 					ORDER
 				</Button>
