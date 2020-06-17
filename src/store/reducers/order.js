09f30249303d9f96762d06c4b9644c5f8952da27
Olesia.G <olgarbuzova@gmail.com>
2020-06-17 16:04:40 -0400

    refactored order reducers

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -7,39 +7,59 @@ const initialState = {
 	purchased: false,
 }
 
-const reducer = (state = initialState, action) => {
-	switch (action.type) {
-		case actionTypes.PURCHASE_BURGER_SUCCESS:
-			const newOrder = updateObject(action.orderData, {id: action.orderId})
+const purchaseInit = (state, action) =>{
+return updateObject(state, { purchased: false })
+	}
+	
+const purchaseBurgerStart = (state, action) => {
+		return updateObject(state, { loading: true })
+}
+
+const purchaseBurgerSuccess = (state, action) =>{
+	const newOrder = updateObject(action.orderData, {id: action.orderId})
 			return updateObject(state, {
 				loading: false,
 				orders: state.orders.concat(newOrder),
 				purchased: true,
 			})
+}
 
-		case actionTypes.PURCHASE_BURDER_FAIL:
-			return updateObject(state, { loading: false })
+const purchaseBurgerFailed = (state, action) => {
+	return updateObject(state, { loading: false })
+}
 
-		case actionTypes.PURCHASE_BURGER_START:
-			return updateObject(state, { loading: true })
-
-		case actionTypes.PURCHASE_INIT:
-			return updateObject(state, { purchased: false })
-
-		case actionTypes.FETCH_ORDERS_START:
-			return updateObject(state, { loading: true })
-
-		case actionTypes.FETCH_ORDERS_SUCCESS:
-			return updateObject(state, {
+const fetchOrdersSuccess = (state, action) => {
+		return updateObject(state, {
 				orders: action.orders,
 				loading: false,
 			})
+}
 
-		case actionTypes.FETCH_ORDERS_FAILED:
-			return updateObject(state, { loading: false })
+const fetchOrdersStart = (state, action) => {
+	return updateObject(state, { loading: true })
+}
 
-		default:
-			return state
+const fetchOrdersFailed = (state, action) => {
+	return updateObject(state, { loading: false })
+}
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action)
+		
+		case actionTypes.PURCHASE_BURDER_FAIL: return purchaseBurgerFailed(state, action)
+			
+		case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action)
+		
+		case actionTypes.PURCHASE_INIT: return purchaseInit(state, action)
+			
+		case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state, action)
+	
+		case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action)
+		
+		case actionTypes.FETCH_ORDERS_FAILED: return fetchOrdersFailed(state, action)
+		
+ 		default: return state
 	}
 }
 
